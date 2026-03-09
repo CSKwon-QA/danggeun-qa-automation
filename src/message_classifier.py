4f54745e86f202c5283feeb59ba5de7c3957ab76
CSKwon-QA <ckstjvneld98@gmail.com>
2026-03-09 21:25:34 +0900

    Update message_classifier.py

--- src/message_classifier.py
+++ src/message_classifier.py
@@ -1,1 +1,7 @@
-
+def classify_message(message):
+    if message.startswith("[Server]"):
+        return "server"
+    elif message.startswith("[Client]"):
+        return "client"
+    else:
+        return "unknown"
